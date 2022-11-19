8954f6b019ed9d9587e7da5d599e44582ae75c12
bala35210 <bala.selvaraji@gmail.com>
2022-11-19 17:12:34 +0530

    Uploaded Project Video

--- Project Development Phase/Sprint 4/Liver_Flask_App.py
+++ Project Development Phase/Sprint 4/Liver_Flask_App.py
@@ -7,7 +7,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
-
+import sklearn
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
